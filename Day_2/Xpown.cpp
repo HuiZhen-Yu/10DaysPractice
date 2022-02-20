5a680d536b0102a6138b0ae5aef7778e35d70982
HuiZhenYu <1968795553@qq.com>
2022-02-20 16:15:21 +0800

    day2

---
+++ Day_2/Xpown.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+double mypower(double x, double y) {
+    
+}
+
+double power(double x, int n)
+{
+    double val = 1.0;
+    while (n--)
+    {
+        val *= x;
+    }
+    return val;
+}
+
+int main(int argc, char const *argv[])
+{
+    int x;
+    cin >> x;   //需要输入二进制的数10101100->179
+    int wei = 0;
+    int sum = 0;
+    int each, chu;
+    for (int i = 0; i < 8; i++)
+    {
+        each = x % 10;
+        chu = x / 10;
+        sum += each * power(2, wei);
+        x = chu;
+        wei++;
+    }
+    cout << sum << endl;
+    system("pause");
+    return 0;
+}
