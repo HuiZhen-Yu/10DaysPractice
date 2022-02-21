36762ea641134ffbf33b26b39450f67d17c4510e
HuiZhenYu <1968795553@qq.com>
2022-02-21 10:31:29 +0800

    Day2

--- Day_2/Xpown.cpp
+++ Day_2/Xpown.cpp
@@ -1,36 +1,44 @@
 #include <iostream>
 using namespace std;
 
-double mypower(double x, double y) {
-    
-}
-
-double power(double x, int n)
+/**
+ * 计算x的y次幂
+ **/
+double power(const double &x, int y)
 {
-    double val = 1.0;
-    while (n--)
+    double value = 1;
+    while (y--)
     {
-        val *= x;
+        value *= x;
     }
-    return val;
+    return value;
 }
 
-int main(int argc, char const *argv[])
+int main(int argc, char **argv)
 {
-    int x;
-    cin >> x;   //需要输入二进制的数10101100->179
-    int wei = 0;
-    int sum = 0;
-    int each, chu;
+    /* 第一个功能计算x的y次方 */
+    double base = 0.0;
+    int index = 0;
+    cout << "请输入底数: " << endl;
+    cin >> base;
+    cout << "请输入指数 : " << endl;
+    cin >> index;
+    double result = power(base, index);
+    cout << "结果是：" << result << endl;
+
+    /* 第二个功能 将二进制数转换成10进制数 */
+    unsigned int num_binary = 0;
+    unsigned int num_decimal = 0;
+    cout << "请输入二进制序列 : " << endl;
+    cin >> num_binary;
     for (int i = 0; i < 8; i++)
     {
-        each = x % 10;
-        chu = x / 10;
-        sum += each * power(2, wei);
-        x = chu;
-        wei++;
+        unsigned int num_digit = num_binary % 10;
+        num_binary /= 10;
+        num_decimal += num_digit ? power(2, i) : 0;
     }
-    cout << sum << endl;
+    cout << "结果是：" << num_decimal << endl;
+
     system("pause");
     return 0;
 }
